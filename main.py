343934db61b49ec3a0aa58ac10cfe2831ddfb479
kcho <keymoon47@hotmail.com>
2025-07-03 17:10:08 -0700

    Temporarily disable login manager to register first admin

--- main.py
+++ main.py
@@ -37,11 +37,11 @@ ckeditor = CKEditor(app)
 Bootstrap5(app)
 
 # Configure Flask-Login # 🌟🌟 comment out for register first log-in
-# login_manager = LoginManager()
-# login_manager.init_app(app)
-# @login_manager.user_loader
-# def load_user(user_id):
-#     return db.get_or_404(User, user_id)
+login_manager = LoginManager()
+login_manager.init_app(app)
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(User, user_id)
 
 
 # For adding profile images to the comment section
