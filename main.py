0d2f7b3f800bab96b0f74d14182af818cc75d9bb
kcho <keymoon47@hotmail.com>
2025-07-10 10:48:59 -0700

    update index.html 10:48 on 7/10/2025

--- main.py
+++ main.py
@@ -179,7 +179,7 @@ def add_new_post():
         db.session.add(new_post)
         db.session.commit()
         return redirect(url_for("get_all_posts"))
-    return render_template("make-post.html", form=form, current_user=current_user)
+    return render_template("make-post.html", form=form, current_user=current_user, is_edit=False)
 
 @app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
 @admin_only
