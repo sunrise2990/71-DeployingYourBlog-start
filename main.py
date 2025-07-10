991cef300dd252f6cedb77bfe8948d2cf6a860d0
kcho <keymoon47@hotmail.com>
2025-07-10 11:19:13 -0700

    update index.html 11:18 on 7/10/2025

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ with app.app_context():
 def admin_only(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if current_user.id != 1:
+        if not current_user.is_authenticated or current_user.id != 1:
             return abort(403)
         return f(*args, **kwargs)
     return decorated_function
