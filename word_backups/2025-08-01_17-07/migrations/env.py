24c84d2633ad25ae15d8b63ab3082ccc8fee95a4
kcho <keymoon47@hotmail.com>
2025-08-01 17:12:01 -0700

    test – scenario db - 8/1/2025 5:11 PM

--- word_backups/2025-08-01_17-07/migrations/env.py
+++ word_backups/2025-08-01_17-07/migrations/env.py
@@ -2,10 +2,11 @@ import logging
 from logging.config import fileConfig
 
 from flask import current_app
+
 from alembic import context
 
-# This is the Alembic Config object, which provides
-# access to values within the .ini file in use.
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
 config = context.config
 
 # Interpret the config file for Python logging.
@@ -15,56 +16,56 @@ logger = logging.getLogger('alembic.env')
 
 
 def get_engine():
-    """
-    Get the SQLAlchemy engine instance from Flask-Migrate's db extension.
-    Supports both Flask-SQLAlchemy < 3 and >= 3.
-    """
     try:
-        # For Flask-SQLAlchemy < 3 and Alchemical
+        # this works with Flask-SQLAlchemy<3 and Alchemical
         return current_app.extensions['migrate'].db.get_engine()
     except (TypeError, AttributeError):
-        # For Flask-SQLAlchemy >= 3
+        # this works with Flask-SQLAlchemy>=3
         return current_app.extensions['migrate'].db.engine
 
 
 def get_engine_url():
-    """
-    Return the database URL string, unmasking the password for Alembic config.
-    """
     try:
-        return get_engine().url.render_as_string(hide_password=False).replace('%', '%%')
+        return get_engine().url.render_as_string(hide_password=False).replace(
+            '%', '%%')
     except AttributeError:
         return str(get_engine().url).replace('%', '%%')
 
 
-# Add your model's MetaData object here for 'autogenerate' support.
-# For example:
+# add your model's MetaData object here
+# for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 config.set_main_option('sqlalchemy.url', get_engine_url())
 target_db = current_app.extensions['migrate'].db
 
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.
+
 
 def get_metadata():
-    """
-    Return the MetaData instance used by SQLAlchemy for migrations.
-    """
     if hasattr(target_db, 'metadatas'):
-        # For newer SQLAlchemy versions with multiple metadata support
         return target_db.metadatas[None]
     return target_db.metadata
 
 
 def run_migrations_offline():
-    """
-    Run migrations in 'offline' mode.
-    No DBAPI connection required.
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
     """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
-        url=url,
-        target_metadata=get_metadata(),
-        literal_binds=True
+        url=url, target_metadata=get_metadata(), literal_binds=True
     )
 
     with context.begin_transaction():
@@ -72,15 +73,17 @@ def run_migrations_offline():
 
 
 def run_migrations_online():
-    """
-    Run migrations in 'online' mode.
-    Creates Engine and associates connection with context.
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
     """
 
+    # this callback is used to prevent an auto-migration from being generated
+    # when there are no changes to the schema
+    # reference: http://alembic.zzzcomputing.com/en/latest/cookbook.html
     def process_revision_directives(context, revision, directives):
-        """
-        Prevent generation of empty migrations.
-        """
         if getattr(config.cmd_opts, 'autogenerate', False):
             script = directives[0]
             if script.upgrade_ops.is_empty():
@@ -108,4 +111,3 @@ if context.is_offline_mode():
     run_migrations_offline()
 else:
     run_migrations_online()
-
